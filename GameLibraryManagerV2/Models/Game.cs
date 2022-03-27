d7424784340d00b4591bbb0a714566ce2da43823
Roland Doll <rdoll007@gmail.com>
2022-03-27 13:54:54 -0400

    minor tweaks

--- GameLibraryManagerV2/Models/Game.cs
+++ GameLibraryManagerV2/Models/Game.cs
@@ -6,9 +6,9 @@ namespace GameLibraryManagerV2.Models
         public Guid Id { get; set; }
         public String Title { get; set; }
         public Status Status { get; set; }
-        public String Console { get; set; }
+        public Console Console { get; set; }
         public Format Format { get; set; }
-        public String Genre { get; set; }
+        public Genre Genre { get; set; }
         public int AverageLength { get; set; }
         public int YearOfRelease { get; set; }
         public DateTime LoggedAt { get; set; } = DateTime.UtcNow;
